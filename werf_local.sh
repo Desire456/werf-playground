f0fd1bd43adc77b2c82fd96fce58cbca56cf1b37
Vladislav Selitskiy <v.selitskiy@breezeventures.io>
2025-09-16 11:08:22 +0400

    initl

---
+++ werf_local.sh
@@ -0,0 +1,6 @@
+export WERF_REPO=localhost:5001/werf-playground;
+export WERF_ENV=rok-test;
+export WERF_DEBUG_TEMPLATES=true;
+export WERF_DEV=true;
+export WERF_KUBE_CONTEXT=docker-desktop;
+#export CHANGELOG_DIR=rok/pg-payments;
